f92ebbf7537935959cdac2d037eddebc43e2a269
xenik <xenikSeefka@gmail.com>
2013-06-17 13:47:09 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GospodaOkna::Application.config.secret_token = 'b4e209fa717c7c02806a715282577038f9c3eff2df3538617b7848322e9e145350176774177bd3f9b625fc165004205da19ed73abfc0b56e6dd53a9456a4e94d'
